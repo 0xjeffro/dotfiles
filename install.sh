b5f73cbc0ba430750f859d64c17feafcaff9e7a7
Jeffro <0xjeffro@gmail.com>
2025-08-30 19:38:15 -0700

    add tmux

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/nvim ~/.config/nvim
 ln -sf $(pwd)/.zshrc ~/.zshrc
 
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+tmux source-file "$HOME/.tmux.conf"
+
 echo "✅ Dotfiles linked!"
